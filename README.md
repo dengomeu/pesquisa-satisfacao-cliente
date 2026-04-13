fe4ec0c852a1659fbc5f518075dfa6ed706491dd
Denis Gomes <268496886+dengomeu@users.noreply.github.com>
2026-04-13 01:07:17 -0300

    feat: implementação de testes com 10 entrevistados

---
+++ README.md
@@ -0,0 +1,23 @@
+# Projeto – Sistema de Pesquisa de Satisfação - TudoWeb 🌐
+
+Um programa interativo via terminal desenvolvido em Python para automatizar a coleta e análise de feedback de clientes da agência TudoWeb.
+
+Este projeto foi desenvolvido com o objetivo de otimizar o processo de pesquisa de opinião. O sistema substitui formulários manuais por um fluxo automatizado que coleta os dados dos entrevistados, valida as entradas em tempo real e gera um relatório estatístico completo ao final da execução.
+
+Para a **fase de testes** o código foi inicialmente configurado para capturar a coleta de 10 entrevistados. Na versão final, alterado para 50 entrevistados.
+
+---
+
+## 🧮 Funcionalidades do programa
+1. **Cadastro de feedback:** O sistema troca ideia com o cliente, pedindo nome, idade e a nota para o atendimento de forma organizada.
+2. **Validação:** Utilizou-se a estrutura _while_ que atua como um filtro: se a opção for inválida, o sistema pede a correção na hora, garantindo que nenhum dado errado estrague o resultado final.
+3. **Cíclico:** O código é programado para rodar o número exato de entrevistas (10 entrevistas na fase de testes e 50 na fase final).
+
+### 🚀 Instruções de uso
+1. Certifique-se de ter o Python instalado.
+2. Clone este repositório ou faça o download do arquivo `pesquisa-satisfacao.py`.
+3. Execute o comando: `pesquisa-satisfacao.py`
+4. Siga as instruções na tela para inserir os dados do aparelho.
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
---
+++ pesquisa-satisfacao.py
@@ -0,0 +1,54 @@
+# Programa de pesquisa de satisfação ao cliente - TudoWeb
+# Coleta de opinião de entrevistados sobre o atendimento
+
+# Variáveis para contabilizar respostas:
+quantidade_excelente = 0
+quantidade_bom = 0
+quantidade_ruim = 0
+
+# Número de entrevistados
+numero_entrevistados = 10 # Ajustado para a fase de testes
+
+for i in range(numero_entrevistados):
+    print(f"\n--- 👤 Entrevistado {i + 1} ---")
+    nome = input("Digite o seu nome: ")
+    idade = input("Digite a sua idade: ")
+
+    print("Avalie nosso atendimento:")
+    print("🟢 [1] - Excelente")
+    print("🟡 [2] - Bom")
+    print("🔴 [3] - Ruim")
+    opiniao = int(input("Digite a sua opinião (1, 2 ou 3): "))
+    
+    while opiniao < 1 or opiniao > 3:
+        print("⚠️ Opção inválida! Por favor, digite apenas 1, 2 ou 3.")
+        opiniao = int(input("\n Digite a sua opinião (1, 2 ou 3): "))
+
+    if opiniao == 1:
+        quantidade_excelente += 1
+        resposta = "Excelente"
+    elif opiniao == 3:
+        quantidade_ruim += 1
+        resposta = "Ruim"
+    elif opiniao == 2:
+        quantidade_bom += 1
+        resposta = "Bom"
+
+    print(f"✅ Registrado: {nome}, {idade} anos - Opinião: {resposta}")
+
+# Exibição dos resultados finais
+print("\n=== 📈 Resultado da pesquisa 📈===")
+print(f"\n 👥 Total de entrevistados: {numero_entrevistados}")
+print("📌 Quantidade de respostas:")
+print(f"  🟢 Excelente: {quantidade_excelente}")
+print(f"  🟡 Bom:       {quantidade_bom}")
+print(f"  🔴 Ruim:      {quantidade_ruim}\n")
+
+percentual_excelente = (quantidade_excelente / numero_entrevistados) * 100
+percentual_bom = (quantidade_bom / numero_entrevistados) * 100
+percentual_ruim = (quantidade_ruim / numero_entrevistados) * 100
+
+print(f"\n 📊 Percentuais:")
+print(f"  Excelente: {percentual_excelente:.1f}%")
+print(f"  Bom: {percentual_bom:.1f}%")
+print(f"  Ruim: {percentual_ruim:.1f}%")
